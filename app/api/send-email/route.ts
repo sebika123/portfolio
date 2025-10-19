726319b42ff0c494a6c0cf855f067f30b92dcd88
sebika nepal <72345247+sebika123@users.noreply.github.com>
2025-10-19 23:08:27 +0545

    bug fixes

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,7 +1,8 @@
 
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_DcsqFb62_LjFpRsBnxkYBgu9CBFYXwLue');
 
 export async function POST(req: Request) {
   const data = await req.json();
